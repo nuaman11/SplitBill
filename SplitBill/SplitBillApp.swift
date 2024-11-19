019a7ead7c38d90eff332714892cf9399f2b5cb8
nuaman <nuaman.s@axeltechnologies.com>
2024-11-19 12:40:50 +0530

    Initial Commit

---
+++ SplitBill/SplitBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillApp.swift
+//  SplitBill
+//
+//  Created by Muhammed Nuaman Sadiq on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
